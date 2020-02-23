5effdc72829bcb6ee1b71bf54f9f2b3b98b71e51
VenkatPantham <venkatpantham5568@gmail.com>
2020-02-23 20:17:58 +0530

    Changed default map option

--- store_data.php
+++ store_data.php
@@ -1,8 +1,8 @@
 <?php
 
 # Connect to PostgreSQL database
-// $conn = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=leaflet;", "postgres", "postgresql");
-$conn = new PDO("pgsql:host=ec2-184-72-236-3.compute-1.amazonaws.com;port=5432;dbname=d1f6ahsj5b8ids", "omhmsgdadziael", "40852ceb183c08bf075e59f067158f485c1e4f1fc1162b0a9ff9bf16e4710c8f");
+$conn = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=leaflet;", "postgres", "postgresql");
+// $conn = new PDO("pgsql:host=ec2-184-72-236-3.compute-1.amazonaws.com;port=5432;dbname=d1f6ahsj5b8ids", "omhmsgdadziael", "40852ceb183c08bf075e59f067158f485c1e4f1fc1162b0a9ff9bf16e4710c8f");
 
 # Checking Connection
 if (!$conn) {
