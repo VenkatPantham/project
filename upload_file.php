8dac09cdfa40ae31295219210ec8b69887c3af94
VenkatPantham <venkatpantham5568@gmail.com>
2020-02-23 20:42:19 +0530

    Changed upload location

--- upload_file.php
+++ upload_file.php
@@ -5,7 +5,7 @@ $uploadOk = 1;
 $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 # Connect to Postgres database
-$conn = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=leaflet;", "postgres", "postgresql");
+$conn = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=trial;", "postgres", "postgresql");
 // $conn = new PDO("pgsql:host=ec2-184-72-236-3.compute-1.amazonaws.com;port=5432;dbname=d1f6ahsj5b8ids", "omhmsgdadziael", "40852ceb183c08bf075e59f067158f485c1e4f1fc1162b0a9ff9bf16e4710c8f");
 
 # Checking Connection
@@ -42,7 +42,8 @@ if ($uploadOk == 0) {
         fclose($file);
 
         # However the User's Query will be passed to the DB:
-        $sql = "SELECT load_csv_file('$tableName','/home/venkat-pantham/Desktop/Project/gis/$target_file',$tableRows)";
+        // $sql = "SELECT load_csv_file('$tableName','/home/venkat-pantham/Desktop/Project/gis/$target_file',$tableRows)";
+        $sql = "SELECT load_csv_file('$tableName','/home/ubuntu/project/$target_file',$tableRows)";
 
         # Try query or error
         $db = $conn->query($sql);
